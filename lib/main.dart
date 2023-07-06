2313e8c162c1860bccc906bd90b218e339f55837
RaphaelNCP <raphael.ofc11@gmail.com>
2023-07-06 20:54:43 -0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -21,13 +21,15 @@ class MyApp extends StatelessWidget {
           foregroundColor: Colors.white,
           title: const Text("Tarefas"),
         ),
-        body: const Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
+        body: ListView(
+          children: const [
             Task("Aprender Flutter"),
             Task("Jogar RPG"),
             Task("Ver Serie"),
-            
+            Task("Ver Serie"),
+            Task("Ver Serie"),
+            Task("Ver Serie"),
+            Task("Ver Serie"),
           ],
         ),
         floatingActionButton: FloatingActionButton(
@@ -40,30 +42,40 @@ class MyApp extends StatelessWidget {
 
 class Task extends StatelessWidget {
   final String nome;
-  const Task(this.nome,{super.key});
+  const Task(this.nome, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(color: Colors.blue, height: 140),
-        Container(
-          color: Colors.white,
-          height: 100,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-            Container(
-              color: Colors.black26,
-              width: 72,
-              height: 100,
-            ),
-            Text(nome),
-            ElevatedButton(
-                onPressed: () {}, child: const Icon(Icons.arrow_drop_up))
-          ]),
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Stack(
+        children: [
+          Container(color: Colors.blue, height: 140),
+          Container(
+            color: Colors.white,
+            height: 100,
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    color: Colors.black26,
+                    width: 72,
+                    height: 100,
+                  ),
+                  SizedBox(
+                    width: 200,
+                    child: Text(
+                      nome,
+                      style: const TextStyle(fontSize: 24,),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  ElevatedButton(
+                      onPressed: () {}, child: const Icon(Icons.arrow_drop_up))
+                ]),
+          ),
+        ],
+      ),
     );
   }
 }
